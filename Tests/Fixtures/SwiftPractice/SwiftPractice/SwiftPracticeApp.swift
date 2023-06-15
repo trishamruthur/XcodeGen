72057e73f533350b889cec139d87ae0aae5ec2de
Trishan Amruthur <136016455+trishamruthur@users.noreply.github.com>
2023-06-15 11:54:40 -0700

    Add files via upload

---
+++ Tests/Fixtures/SwiftPractice/SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by Trishan Amruthur on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
